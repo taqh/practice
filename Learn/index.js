41afe398e98ab75efa2481ef9914e34b17d11bd3
Dom-iha <taqib.ibrahim@gmail.com>
2022-12-27 01:49:38 +0100

    split the js code used for practice from the main code, left it there for reference

--- Learn/index.js
+++ Learn/index.js
@@ -1,104 +1,52 @@
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items')
 
-//   console.dir(document);
+//  Form submitt event  //
+form.addEventListener('submit', addItem);
 
-//  console.log(document.domain);
-//   console.log(document.URL);
-//   console.log(document.title);
+// Add item //
+function addItem(e) {
+    e.preventDefault();
 
-//   console.log(document.forms);
+    //  Get input value
+    let newItem = document.getElementById('item').value;
 
-// console.log(document.getElementById('header-title'));
-// let headerTitle = document.getElementById('header-title')
-// console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
+    //  Create new li element  //
+    let li = document.createElement('li');
 
-// headerTitle.style.borderBottom = 'solid 3px red';
+    // Add class  //
+    li.className = 'list-group-item';
 
-// let items = document.getElementsByClassName('list-group-item');
-// console.log(items)
-// console.log(items[2])
+    //  Add text node with input value //
+    li.appendChild(document.createTextNode(newItem));
 
-// items[1].textContent = 'Hello 2';
-// items[1].style.backgroundColor = 'yellow'
+    //  Create delete button element  //
+    let deleteBtn = document.createElement('button');
 
+    // Add classes to delete button //
+    deleteBtn.className = 'delete';
 
-//GETELEMENTSBYTAGNAME
+    //  Append text Node  //
+    deleteBtn.appendChild(document.createTextNode('X'));
 
+    //  Append button to li  //
+    li.appendChild(deleteBtn);
 
-
-// QUERYSELECTOR
-
-// let header = document.querySelector('#main-header');
-// header.style.borderBottom = '4px solid red';
-
-// let input = document.querySelector('input');
-// input.value = 'Hello World'
-
-// let submit = document.querySelector('input[type="submit"]');
-// submit.value="SEND"
-
-// let item = document.querySelector('.list-group-item');
-// item.style.color = 'red';
-
-// let lastItem = document.querySelector
-// ('.list-group-item:last-child');
-// lastItem.style.color = 'blue'
-
-// let seconditem = document,querySelector
-// ('.list-group-item:nth-child(2)');
-// seconditem.style.color = 'coral'
+    //  Append li to list   //
+    itemList.appendChild(li);
+}
 
 
-// //QUERYSELECCTOR ALL//
+//  Delete event  //
+itemList.addEventListener('click', removeItem);
 
-// let titles = document.querySelectorAll('.title');
+// Remove item  //
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')) {
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
 
-// console.log(titles);
-
-// let odd = document.querySelectorAll('li:nth-child(odd)');
-
-// for (let i = 0; i < odd.length; i++) {
-//     odd[i]
-// }
-
-
-
-//Traversing the dom //
-
-let itemList = document.querySelector('#items');
-//parentNode
-// console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentNode.parentNode.parentNode);
-
-// console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentElement.parentElement.parentElement);
-
-// childNodes
-// console.log(itemList.childNodes);
-
-// console.log(itemList.children);
-// console.log(itemList.children[2]);
-
-// itemList.children[2].style.backgroundColor = 'burlywood';
-
-// FirstChild
-// console.log(itemList.firstChild);
-// //FirstElementChild
-// console.log(itemList.firstElementChild);
-// itemList.firstElementChild.textContent = 'Hello 1';
-
-
-// LastChild
-// console.log(itemList.lastChild);
-//LastElementChild
-// console.log(itemList.lastElementChild);
-// itemList.lastElementChild.textContent = 'Hello 1';
-
-
-// nextSibling
-// console.log(itemList.nextSibling)
-
-// nextElementSIbling
-console.log(itemList.nextElementSibling)
+} 
