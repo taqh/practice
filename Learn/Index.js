bd3ffabc64e53dbdb1135e574777dfcdff126021
Dom-iha <taqib.ibrahim@gmail.com>
2022-12-31 21:58:36 +0100

    added my payment flow practice project

--- Learn/Index.js
+++ Learn/Index.js
@@ -112,4 +112,27 @@
 
 // console.log(PI);
 
-const magic = () => new Date();
+// const magic = () => new Date();
+
+// let myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+// console.log(myConcat([1, 2], [3, 4, 5]));
+
+// const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+// const squareList = (arr) => {
+//     const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+//     return squaredIntegers;
+// };
+
+// const squaredIntegers = squareList(realNumberArray);
+// console.log(squaredIntegers);
+
+const increment = (function(){
+    return function increment(number, value = 1) {
+        return number + value;
+    };
+}) ();
+
+console.log(increment(5, 2));
+console.log(increment(5));
