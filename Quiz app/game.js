d3bab2c2139cb3785ce26a740698d01df2d41fc1
Dom-iha <taqib.ibrahim@gmail.com>
2023-01-15 00:57:27 +0100

    added loading screen

--- Quiz app/game.js
+++ Quiz app/game.js
@@ -4,6 +4,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game'); 
 
 let currentQuestion = {}
 let acceptingAnswers = false;
@@ -11,32 +13,34 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the javaScript ??",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-    }
-];
+let questions = [];
+
+fetch("https://opentdb.com/api.php?amount=10&category=9")
+.then(res => {
+    return res.json();
+}).then(loadedQuestions => {
+    console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map( loadedQuestion => {
+        const formattedQuestion = {
+            question: loadedQuestion.question
+        };
+
+       const answerChoices = [...loadedQuestion.incorrect_answers];
+       formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+        answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
+
+        answerChoices.forEach((choice, index) => {  
+            formattedQuestion["choice" + (index + 1)] = choice;
+        })
+
+        return formattedQuestion;
+    });
+
+     startGame();
+})
+.catch( err => {
+    console.log(err);
+});
 
 // Constant
 const CORRECT_BONUS = 10;
@@ -47,6 +51,8 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
@@ -109,4 +115,3 @@ incrementScore = num => {
     score += num;
     scoreText.innerText = score
 }
-startGame();
