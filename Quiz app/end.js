0e94064f52d43bbf08cddc2dc95b7f4b51326947
Dom-iha <taqib.ibrahim@gmail.com>
2023-01-14 17:02:12 +0100

    highscores page

--- Quiz app/end.js
+++ Quiz app/end.js
@@ -3,16 +3,31 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore =document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores = JSON.parse(localStorage.getItem("highScores"));
-console.log(highScores);
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    console.log(username.value);
     saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
     console.log("clicked the save button");
     e.preventDefault();
-}
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign("index.html")
+    
+};
