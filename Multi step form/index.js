432622205d1a5c6d7a29617af636565e51b712db
Dom-iha <taqib.ibrahim@gmail.com>
2023-01-19 21:06:26 +0100

    form validation

--- Multi step form/index.js
+++ Multi step form/index.js
@@ -1,14 +1,40 @@
 
-// FORM VALIDATION
-let form = document.querySelector('form');
-let input = document.querySelector('input')
+// const form = document.querySelector('form');
+// const input = document.querySelector('input')
+
+// console.log(form);
+// form.addEventListener('submit', (e) =>  {
+//     e.preventDefault();
+
+//     let validate = document.querySelector('.invalid')
+//     if (input.value ==='' ) {
+//         validate.removeAttribute('hidden')
+//         input.classList.add('required');
+//     } else {
+//         validate.setAttribute('hidden','')
+//         input.classList.remove('required');
+//     }
+//  });
 
 
-form.addEventListener('submit', (e) =>  {
-    e.preventDefault();
-    let validate = document.querySelector('.invalid')
-    if (input.value ==='' ) {
-        validate.removeAttribute('hidden')
-        input.className = 'required';
+
+const inputs = document.querySelectorAll('input');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  inputs.forEach((input) => {
+    const validate = input.parentNode.querySelector('.invalid')
+    
+    if (!input.value) {
+      validate.removeAttribute('hidden');
+      input.classList.add('required');
+    } else {
+      validate.setAttribute('hidden','');
+      input.classList.remove('required');
     }
+  });
 });
+
+form.reset();
