ff938b52dcfdc6af73aa061ac29da9ec23b78d68
Dom-iha <taqib.ibrahim@gmail.com>
2023-01-22 22:49:17 +0100

    tweaks

--- Multi step form/index.js
+++ Multi step form/index.js
@@ -1,40 +1,56 @@
+// ----- Form Validation ----- //
 
-// const form = document.querySelector('form');
-// const input = document.querySelector('input')
-
-// console.log(form);
-// form.addEventListener('submit', (e) =>  {
-//     e.preventDefault();
-
-//     let validate = document.querySelector('.invalid')
-//     if (input.value ==='' ) {
-//         validate.removeAttribute('hidden')
-//         input.classList.add('required');
-//     } else {
-//         validate.setAttribute('hidden','')
-//         input.classList.remove('required');
-//     }
-//  });
-
-
-
-const inputs = document.querySelectorAll('input');
 const form = document.querySelector('form');
+const input = document.querySelector('input')
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  inputs.forEach((input) => {
-    const validate = input.parentNode.querySelector('.invalid')
-    
-    if (!input.value) {
-      validate.removeAttribute('hidden');
-      input.classList.add('required');
+console.log(form);
+form.addEventListener('submit', (e) =>  {
+    e.preventDefault();
+
+    let validate = document.querySelector('.invalid')
+    if (input.value ==='' ) {
+        validate.removeAttribute('hidden')
+        input.classList.add('required');
     } else {
-      validate.setAttribute('hidden','');
-      input.classList.remove('required');
+        validate.setAttribute('hidden','')
+        input.classList.remove('required');
     }
-  });
-});
+ });
+
+
+
+// const inputs = document.querySelectorAll('input');
+// const form = document.querySelector('form');
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+  
+//   inputs.forEach((input) => {
+//     const validate = input.parentNode.querySelector('.invalid')
+    
+//     if (!input.value) {
+//       validate.removeAttribute('hidden');
+//       input.classList.add('required');
+//     } else {
+//       validate.setAttribute('hidden','');
+//       input.classList.remove('required');
+//     }
+//   });
+// });
 
 form.reset();
+
+
+// ----- Switch sections ----- //
+// const next = document.querySelector('.btn')
+// const plans = document.getElementById('plans')
+// const info = document.getElementById('info')
+
+// Btn.addEventListener('click', (e) => {
+  
+//   let input = document.querySelector('.login');
+//   if(input.value) {
+//     info.setAttribute('hidden','');
+//     plans.removeAttribute('hidden');
+//   }
+// })
