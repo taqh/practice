d6afc3687adf1989d9e8caa9459fac2b18a61df2
Dom-iha <taqib.ibrahim@gmail.com>
2023-10-03 02:19:39 +0100

    added Root.jsx

---
+++ countries/src/routes/Root.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+
+function Root() {
+   return (
+      <div id='app' className='min-h-screen dark:bg-DarkBg transition-colors duration-300'>
+         <Header />
+         <main className='grid gap-12 max-w-[87.5rem] min-h-[80vh] mx-auto p-6 md:px-14  text-DarkBg dark:text-White transition-colors duration-300 '>
+            <Outlet />
+         </main>
+      </div>
+   );
+}
+
+export default Root;
