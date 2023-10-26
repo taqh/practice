20c2f4c897a919e9f341c363806a6a5e5494aa99
Taqib Ibrahim <106284691+Dom-iha@users.noreply.github.com>
2023-10-27 00:19:23 +0100

    renamed stuff

--- README.md
+++ README.md
@@ -47,7 +47,7 @@
   <tr>
     <td>Space tourism website v2</td>
     <td><a href="https://www.frontendmentor.io/solutions/space-tourism-multipage-website-uxKXi_S_rL">Solution</a></td>
-    <td><a href="https://spacetourweb.netlify.app">Live site</a></td>
+    <td><a href="https://spacetourlive.netlify.app">Live site</a></td>
   </tr>
   <tr>
     <td>Social media dashboard</td>
--- countries/src/components/DetailSkeleton.jsx
+++ countries/src/components/DetailSkeleton.jsx
@@ -7,7 +7,7 @@ export const DetailSkeleton = () => {
             <div className='skeleton skeleton-image'></div>
          </div>
          <div className='grid md:grid-cols-2 gap-6 md:grid-rows-[auto_1fr_auto] text-DarkBg dark:text-White transition duration-300'>
-               <div className='skeleton name md:col-span-2'></div>
+               <div className='skeleton skeleton-heading md:col-span-2'></div>
             <div className='col-start-1'>
                <div className='skeleton skeleton-text'></div>
                <div className='skeleton skeleton-text'></div>
@@ -23,10 +23,10 @@ export const DetailSkeleton = () => {
             <div className='flex flex-col lg:flex-row gap-3 md:col-span-2 md:items-center'>
                <div className='skeleton skeleton-label'></div>
                <div className='flex gap-3 flex-wrap'>
-                  <div className='skeleton border-country'></div>
-                  <div className='skeleton border-country'></div>
-                  <div className='skeleton border-country'></div>
-                  <div className='skeleton border-country'></div>
+                  <div className='skeleton skeleton-item'></div>
+                  <div className='skeleton skeleton-item'></div>
+                  <div className='skeleton skeleton-item'></div>
+                  <div className='skeleton skeleton-item'></div>
                </div>
             </div>
          </div>
