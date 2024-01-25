b1ce556d27c800932fad96d3b153df6ca1bd87a8
Dom-iha <taqib.ibrahim@gmail.com>
2024-01-25 23:53:39 +0100

    completed markup

--- interactive-rating-component/src/main.ts
+++ interactive-rating-component/src/main.ts
@@ -1,6 +1,13 @@
-import './style.css'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import { setupCounter } from './counter.ts';
+
+const form = document.querySelector<HTMLFormElement>('#form');
+const button = document.querySelector<HTMLButtonElement>('.btn');
 
 
+form?.addEventListener('submit', (event: SubmitEvent) => {
+   event.preventDefault();
+   
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('.btn')!);
