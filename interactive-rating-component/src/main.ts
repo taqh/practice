3362cd001bab4f9ad4c57da7ecfd772aec232767
Dom-iha <taqib.ibrahim@gmail.com>
2024-01-26 22:12:48 +0100

    improved component styling

--- interactive-rating-component/src/main.ts
+++ interactive-rating-component/src/main.ts
@@ -3,11 +3,12 @@ import { setupCounter } from './counter.ts';
 
 const form = document.querySelector<HTMLFormElement>('#form');
 const button = document.querySelector<HTMLButtonElement>('.btn');
-
+const card = document.querySelector<HTMLDivElement>('.card');
 
 form?.addEventListener('submit', (event: SubmitEvent) => {
    event.preventDefault();
    
+   card?.setAttribute('hidden', 'true')
 });
 
 // setupCounter(document.querySelector<HTMLButtonElement>('.btn')!);
