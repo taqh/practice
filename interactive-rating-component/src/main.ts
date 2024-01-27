7048c493f2175d610a5052afea2109587a67f760
Dom-iha <taqib.ibrahim@gmail.com>
2024-01-27 22:24:43 +0100

    Added rating functionality and updated styles

--- interactive-rating-component/src/main.ts
+++ interactive-rating-component/src/main.ts
@@ -1,14 +1,29 @@
-import './style.css';
 import { setupCounter } from './counter.ts';
 
 const form = document.querySelector<HTMLFormElement>('#form');
-const button = document.querySelector<HTMLButtonElement>('.btn');
-const card = document.querySelector<HTMLDivElement>('.card');
+const submitBtn = document.querySelector<HTMLButtonElement>('.btn');
+const ratingCard = document.querySelector<HTMLDivElement>('.card--rating');
+const thanksCard = document.querySelector<HTMLDivElement>('.card--thanks');
+const rating = document.querySelector<HTMLParagraphElement>('.rating');
+const ratingInputs = document.querySelectorAll<HTMLInputElement>('.card-form__input');
+
+
+const getRating = () => {
+   let ratingValue: string | number = 0;
+   ratingInputs.forEach((input) => {
+      if (input.checked) {
+         ratingValue = input.value;
+      }
+   });
+   return ratingValue;
+}
 
 form?.addEventListener('submit', (event: SubmitEvent) => {
    event.preventDefault();
    
-   card?.setAttribute('hidden', 'true')
+   ratingCard?.setAttribute('data-visible', 'false')
+   thanksCard?.setAttribute('data-visible', 'true')
+
+   rating ? rating.innerText = `You selected ${getRating()} out of 5` : 'You selected # out of 5';
 });
 
-// setupCounter(document.querySelector<HTMLButtonElement>('.btn')!);
