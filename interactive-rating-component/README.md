3a2a605a86806e189e69e16369707251c5c1830e
Dom-iha <taqib.ibrahim@gmail.com>
2024-03-13 00:32:18 +0100

    fixed issue with overlapping timeouts

--- interactive-rating-component/README.md
+++ interactive-rating-component/README.md
@@ -1,12 +1,11 @@
 # Frontend Mentor - Interactive rating component solution
 
-This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
 
 - [Overview](#overview)
   - [The challenge](#the-challenge)
-  - [Screenshot](#screenshot)
   - [Links](#links)
 - [My process](#my-process)
   - [Built with](#built-with)
@@ -25,14 +24,10 @@ Users should be able to:
 - Select and submit a number rating
 - See the "Thank you" card state after submitting a rating
 
-### Screenshot
-
-![](./screenshot.jpg)
-
 ### Links
 
-- [Solution URL](https://your-solution-url.com)
-- [Live Site URL](https://your-live-site-url.com)
+- [Solution URL](https://www.frontendmentor.io/solutions/interactive-rating-component-bem-css-3d-effect-WeH9BbjsGZ)
+- [Live Site URL](https://support-rating.netlify.app)
 
 ## My process
 
@@ -46,12 +41,13 @@ Users should be able to:
 
 ### What I learned
 
-I learnt a little bit about css 3d properties and how to use them to create a 3d effect on the card.
+I learnt a little bit about CSS 3d properties and how to use them to create a 3d effect on the card.
 
 ### Continued development
 
-I would like to continue learning about css 3d properties and how to use them to create amazing 3d effects.
+I will continue learning about 3d properties in CSS and how to use them to create amazing 3d effects.
 
 ## Author
 
 - Twitter - [@_Mihaq](https://twitter.com/_Mihaq)
+- Linkedin - [@Taqib](https://www.linkedin.com/taqib-ibrahim)
--- interactive-rating-component/src/main.ts
+++ interactive-rating-component/src/main.ts
@@ -1,50 +1,56 @@
 const form = document.querySelector<HTMLFormElement>('.rating-card__form');
-const change = document.querySelector<HTMLButtonElement>('.change');
+const changeRating = document.querySelector<HTMLButtonElement>('.change');
 const ratingCard = document.querySelector<HTMLDivElement>('.rating-card');
 const thanksCard = document.querySelector<HTMLDivElement>('.thanks-card');
 const rating = document.querySelector<HTMLParagraphElement>('.rating');
-const ratingInputs = document.querySelectorAll<HTMLInputElement>('.rating-card__input');
+const ratingInputs = document.querySelectorAll<HTMLInputElement>(  '.rating-card__input');
 const card = document.querySelector<HTMLDivElement>('.card');
+let timeoutId: number;
 
 const getRating = () => {
-   let ratingValue: string = '0';
-   ratingInputs.forEach((input) => {
-      if (input.checked) {
-         ratingValue = input.value;
-      }
-   });
-   return ratingValue;
-}
+  let ratingValue: string = '0';
+  ratingInputs.forEach((input) => {
+    if (input.checked) {
+      ratingValue = input.value;
+    }
+  });
+  return ratingValue;
+};
 
 form?.addEventListener('submit', (event: SubmitEvent) => {
-   event.preventDefault();
-   
-   // add the class that rotates the card
-   card?.classList.add('rotate');
+  event.preventDefault();
 
-   // make the thanks card visible
-   thanksCard?.setAttribute('data-visible', 'true')
+  // add the class that rotates the card
+  card?.classList.add('rotate');
 
-   // add a little delay before removing the rating card
-   setTimeout(() => {
-      ratingCard?.setAttribute('data-visible', 'false')
-   }, 1000);
+  // make the thanks card visible
+  thanksCard?.setAttribute('data-visible', 'true');
 
-   rating ? rating.innerText = `You selected ${getRating()} out of 5` : 'You selected # out of 5';
+  // Clear any existing timeout before setting a new one to avoid setting multiple timeouts and hiding both cards
+  clearTimeout(timeoutId);
+
+  // add a little delay before removing the rating card
+  timeoutId = setTimeout(() => {
+    ratingCard?.setAttribute('data-visible', 'false');
+  }, 600);
+
+  rating
+    ? (rating.innerText = `You selected ${getRating()} out of 5`)
+    : 'You selected # out of 5';
 });
 
+changeRating?.addEventListener('click', () => {
+  // remove the class that rotates the card
+  card?.classList.remove('rotate');
 
-change?.addEventListener('click', () => {
+  // make rating card visible
+  ratingCard?.setAttribute('data-visible', 'true');
 
-   // add the class that rotates the card
-   card?.classList.remove('rotate');
+  // Clear any existing timeout before setting a new one to avoid setting multiple timeouts and hiding both cards
+  clearTimeout(timeoutId);
 
-   // make rating card visible
-   ratingCard?.setAttribute('data-visible', 'true')
-
-   // add a little delay before removing the thanks card
-   setTimeout(() => {
-      thanksCard?.setAttribute('data-visible', 'false')
-   }, 1000);
-})
-
+  // add a little delay before removing the thanks card
+  timeoutId = setTimeout(() => {
+    thanksCard?.setAttribute('data-visible', 'false');
+  }, 600);
+});
